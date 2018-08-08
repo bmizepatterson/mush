3d11263ee2e33df7ffd991f9649829b1b0c414a9
Ben Patterson <bmizepatterson@gmail.com>
2018-08-08 10:57:19 -0400

    Position the canvas on the screen.

--- mush.js
+++ mush.js
@@ -3,3 +3,8 @@ var canvas = document.getElementsByTagName('canvas')[0],
 
 canvas.width = 600;
 canvas.height = 400;
+canvas.style.left = (window.innerWidth - canvas.width)/2+'px';
+
+if (window.innerHeight > canvas.height) {
+	canvas.style.top = (window.innerHeight - canvas.height)/2+'px';
+}
