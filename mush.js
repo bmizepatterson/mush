1750453959c1b918cb480d1903b624687ba40b9c
Ben Patterson <bmizepatterson@gmail.com>
2018-08-08 11:30:10 -0400

    Set default canvas height and width values

--- mush.js
+++ mush.js
@@ -1,10 +1,63 @@
-var canvas = document.getElementsByTagName('canvas')[0],
-	   ctx = canvas.getContext('2d');
+var    canvas = document.getElementsByTagName('canvas')[0],
+	   	  ctx = canvas.getContext('2d'),
+ defaultWidth = 600,
+defaultHeight = 400,
+
 
 canvas.width = 600;
 canvas.height = 400;
 canvas.style.left = (window.innerWidth - canvas.width)/2+'px';
 
-if (window.innerHeight > canvas.height) {
-	canvas.style.top = (window.innerHeight - canvas.height)/2+'px';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function resize() {
+	if (window.innerWidth > canvas.width) {
+		canvas.style.left = (window.innerWidth - canvas.width)/2+'px';
+	} else {
+		canvas.style.left = 0;
+	}
+	if (window.innerHeight > canvas.height) {
+		canvas.style.top = (window.innerHeight - canvas.height)/2+'px';
+	} else {
+		canvas.style.top = 0;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+init();
