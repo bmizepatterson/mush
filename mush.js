1c0fda9323dceddd9c4b8e2c45d5f343e9e7f713
Ben Patterson <bmizepatterson@gmail.com>
2018-08-10 19:33:32 -0400

    Just make lines dance around

--- mush.js
+++ mush.js
@@ -12,17 +12,70 @@ function Curve(startX, startY, endX, endY) {
 	this.startY = startY;
 	this.endX = endX;
 	this.endY = endY;
+	this.slope = (endY-startY)/(endX-startX);
 	this.color = colors[ Math.floor(Math.random() * 5) ];
+	this.startVX = Math.round( Math.random() * 6) - 1.5;
+ 	this.startVY = Math.round( Math.random() * 6) - 1.5;
+ 	this.endVX = Math.round( Math.random() * 6) - 1.5;
+ 	this.endVY = Math.round( Math.random() * 6) - 1.5;
+ 	this.midpointX = Math.round((startX+endX)/2);
+ 	this.midpointY = Math.round((startY+endY)/2);
+ 	this.centerIncrement = 100;
+ 	this.id = Curves.length;
 	Curves.push(this);
 
 	this.draw = function() {
 		ctx.strokeStyle = this.color;
-		ctx.lineWidth = 2;
+		ctx.lineWidth = 1;
+		ctx.shadowColor = this.color;
+		ctx.shadowBlur = 20;
 		ctx.beginPath();
 		ctx.moveTo(this.startX, this.startY);
 		ctx.lineTo(this.endX, this.endY);
 		ctx.stroke();
 		ctx.closePath();
+
+		var length = distance(this.startX, this.startY, this.endX, this.endY);
+		ctx.beginPath();
+		ctx.arc(this.midpointX, this.midpointY, Math.floor(length/30)+1, 0, 2 * Math.PI);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+		ctx.closePath();
+		
+		
+		if (Curves.length > this.id+1) {
+			var next = Curves[this.id+1];
+			var radius = distance(this.midpointX, this.midpointY, next.midpointX, next.midpointY)/2;
+			var centerX = (this.midpointX+next.midpointX)/2;
+			var centerY = (this.midpointY+next.midpointY)/2;
+			ctx.beginPath();
+			// ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+			ctx.moveTo(this.endX, this.endY);
+			ctx.lineTo(next.startX, next.startY);
+			ctx.stroke();
+			ctx.closePath();
+		}
+
+		this.startX += this.startVX;
+		this.startY += this.startVY;
+		this.endX += this.endVX;
+		this.endY += this.endVY;
+
+		this.midpointX = (this.endX+this.startX)/2;
+		this.midpointY = (this.endY+this.startY)/2;
+
+		if (this.startX > canvas.width || this.startX < 0) this.startVX = -this.startVX;
+		// if (this.startX < 0) this.startX = canvas.width;
+		if (this.endX > canvas.width || this.endX < 0) this.endVX = -this.endVX;
+		// if (this.endX < 0) this.endX = canvas.width;
+		if (this.startY > canvas.height || this.startY < 0) this.startVY = -this.startVY;
+		// if (this.startY < 0) this.startY = canvas.height;
+		if (this.endY > canvas.height || this.endY < 0) this.endVY = -this.endVY;
+		// if (this.endY < 0) this.endY = canvas.height;
+	}
+
+	this.evaluate = function(x) {
+		return this.slope * (x - this.endX) + this.endY;
 	}
 }
 
@@ -37,6 +90,7 @@ function init() {
 	canvas.addEventListener('mouseout', canvasMouseOut);
 	canvas.addEventListener('mouseover', canvasMouseOver);
 	document.getElementById('clear').addEventListener('click', clear);
+	document.getElementById('add').addEventListener('click', add);
 	resize();
 }
 
@@ -77,6 +131,11 @@ function canvasMouseMove(event) {
 		currentCurve.endX = x;
 		currentCurve.endY = y;
 	}
+	// Check for collisions
+	// var collision = checkForCollision(x, y);
+	// if (collision instanceof Curve) {
+	// 	collision.highlighted = true;
+	// }
 }
 
 function canvasMouseOver(event) {
@@ -89,8 +148,32 @@ function canvasMouseOut(event) {
 	currentCurve = null;
 }
 
+function checkForCollision(x, y) {
+	var collision = false;
+	for (let i = 0; i < Curves.length; i++) {
+		if (y === Curves[i].evaluate(x)) {
+			collision = Curves[i];
+			break;
+		}
+	}
+	return collision;
+}
+
 function clear() {
 	Curves = [];
+}
+
+function add() {
+	var startX = Math.random() * canvas.width;
+	var endX = Math.random() * canvas.width;
+	var startY = Math.random() * canvas.height;
+	var endY = Math.random() * canvas.height;
+	new Curve(startX, startY, endX, endY);
+}
+
+function distance(x1, y1, x2, y2) {
+	// Use the distance formula to calculate the distance between two points.
+	return Math.sqrt( Math.pow( (x2-x1), 2 ) + Math.pow( (y2-y1), 2 ) );
 }
 
 function getMouseX(x) {
