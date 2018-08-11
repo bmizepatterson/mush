5bedc328957f29bbc0f233160378b65754515723
Ben Patterson <bmizepatterson@gmail.com>
2018-08-10 21:59:00 -0400

    Get the balls moving correctly, sort of

--- mush.js
+++ mush.js
@@ -13,6 +13,7 @@ function Curve(startX, startY, endX, endY) {
 	this.endX = endX;
 	this.endY = endY;
 	this.slope = (endY-startY)/(endX-startX);
+	this.b = this.startY - this.slope * this.startX;
 	this.color = colors[ Math.floor(Math.random() * 5) ];
 	this.startVX = Math.round( Math.random() * 6) - 1.5;
  	this.startVY = Math.round( Math.random() * 6) - 1.5;
@@ -20,7 +21,10 @@ function Curve(startX, startY, endX, endY) {
  	this.endVY = Math.round( Math.random() * 6) - 1.5;
  	this.midpointX = Math.round((startX+endX)/2);
  	this.midpointY = Math.round((startY+endY)/2);
- 	this.centerIncrement = 100;
+ 	this.centerX = (this.endX+this.startX)/2;
+ 	this.centerVX = 1; //Math.round( Math.random() * 6);
+ 	this.centerY = this.slope * this.centerX + this.b;
+ 	this.centerDelta = 100;
  	this.id = Curves.length;
 	Curves.push(this);
 
@@ -37,11 +41,10 @@ function Curve(startX, startY, endX, endY) {
 
 		var length = distance(this.startX, this.startY, this.endX, this.endY);
 		ctx.beginPath();
-		ctx.arc(this.midpointX, this.midpointY, Math.floor(length/30)+1, 0, 2 * Math.PI);
+		ctx.arc(this.centerX, this.centerY, length/30, 0, 2 * Math.PI);
 		ctx.fillStyle = this.color;
 		ctx.fill();
-		ctx.closePath();
-		
+		ctx.closePath();		
 		
 		if (Curves.length > this.id+1) {
 			var next = Curves[this.id+1];
@@ -60,22 +63,52 @@ function Curve(startX, startY, endX, endY) {
 		this.startY += this.startVY;
 		this.endX += this.endVX;
 		this.endY += this.endVY;
-
+		this.centerX += this.centerVX;
+		this.centerY = this.evaluate(this.centerX);
 		this.midpointX = (this.endX+this.startX)/2;
 		this.midpointY = (this.endY+this.startY)/2;
+		this.slope = (this.endY-this.startY)/(this.endX-this.startX);
+		this.b = this.startY - this.slope * this.startX;
 
 		if (this.startX > canvas.width || this.startX < 0) this.startVX = -this.startVX;
-		// if (this.startX < 0) this.startX = canvas.width;
 		if (this.endX > canvas.width || this.endX < 0) this.endVX = -this.endVX;
-		// if (this.endX < 0) this.endX = canvas.width;
 		if (this.startY > canvas.height || this.startY < 0) this.startVY = -this.startVY;
-		// if (this.startY < 0) this.startY = canvas.height;
 		if (this.endY > canvas.height || this.endY < 0) this.endVY = -this.endVY;
-		// if (this.endY < 0) this.endY = canvas.height;
+
+		if (this.startX < this.endX) {
+			if (this.centerX > this.endX) {
+				this.centerX = this.endX;
+				this.centerVX = -this.centerVX;
+			}
+			if (this.centerX < this.startX) {
+				this.centerX = this.startX;
+				this.centerVX = -this.centerVX;
+			}
+		} else if (this.endX < this.startX) {
+			if (this.centerX > this.startX) {
+				this.centerX = this.startX;
+				this.centerVX = -this.centerVX;
+			}
+			if (this.centerX < this.endX) {
+				this.centerX = this.endX;
+				this.centerVX = -this.centerVX;
+			}
+		} else if (this.startX === this.endX) {
+			if (this.startY < this.endY) {
+				if (this.centerY > this.endY) this.centerY = this.endY;
+				if (this.centerY < this.startY) this.centerY = this.startY
+			}
+			if (this.endY < this.startY) {
+				if (this.centerY > this.startY) this.centerY = this.startY;
+				if (this.centerY < this.endY) this.centerY = this.endY;
+			}
+			this.centerX = this.startX;
+		}
+
 	}
 
 	this.evaluate = function(x) {
-		return this.slope * (x - this.endX) + this.endY;
+		return this.slope * x + this.b;
 	}
 }
 
